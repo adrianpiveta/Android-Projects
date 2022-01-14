2b9a4ad317f59ea3ca33f7d15a0364d840987f8a
adrianpiveta <danielpiveta257@gmail.com>
2022-01-13 22:25:07 -0300

    More a view

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -44,4 +45,34 @@ class CrimeListFragment : Fragment() {
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
         return view
     }
+    //ViewHolder armazena referencias a itens views
+    // viewHolder sempre cria itemView
+    private inner class CrimeHolder(view: View)
+        : RecyclerView.ViewHolder(view){
+
+            var titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+            var dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        }
+
+    private inner class CrimeAdapter(var crimes: List<Crime>)
+        : RecyclerView.Adapter<CrimeHolder>(){
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+        : CrimeHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_crime, parent,
+            false)
+                return CrimeHolder(view)
+            }
+
+        override fun getItemCount()= crimes.size
+
+        //Responvável por popular uma Holder com uma posição do crime da lista
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+            val crime=crimes[position]
+            holder.apply {
+                titleTextView.text=crime.title
+                dateTextView.text=crime.date.toString()
+            }
+        }
+        }
 }
