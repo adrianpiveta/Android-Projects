6020e5ef182d82064d5ec36a2e0fa2dc84f40490
adrianpiveta <danielpiveta257@gmail.com>
2022-01-19 11:29:30 -0300

    Seleção de idade com Spinner

--- Kotlin_Com_Android/Calculo_Aposentadoria/app/src/main/java/livrokotlin/com/br/calculoaposentadoria/MainActivity.kt
+++ Kotlin_Com_Android/Calculo_Aposentadoria/app/src/main/java/livrokotlin/com/br/calculoaposentadoria/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
 
         val spn_sexo = findViewById<Spinner>(R.id.spn_sexo)
-        val txt_idade = findViewById<EditText>(R.id.txt_idade)
+        val spn_idade = findViewById<Spinner>(R.id.spn_idade)
         val btn_calcular = findViewById<Button>(R.id.btn_calcular)
         val txt_resultado = findViewById<TextView>(R.id.txt_resultado)
 
@@ -24,19 +24,22 @@ class MainActivity : Activity() {
         spn_sexo.adapter = ArrayAdapter<String>(this, android.R.layout.
         simple_spinner_dropdown_item, listOf("Masculino","Feminino")
         )
-
-        for(i in 1..65){
+        //add idades de 1 a 65 anos a lista
+        for(i in 0..65){
             list_idades.add(i)
         }
+        spn_idade.adapter=ArrayAdapter<Int>(this, android.R.layout.
+        simple_spinner_dropdown_item, list_idades)
+
+
+
 
         Log.d("lista",list_idades.toString())
 
         btn_calcular.setOnClickListener {
             val sexo = spn_sexo.selectedItem as String
-            var idade=0
-            idade = txt_idade.text.toString().toInt()
+            val idade=spn_idade.selectedItem as Int
             if(idade != null){
-                Log.d("batata",txt_idade.text.toString().toInt().toString())
                 var resultado=0
                 if(sexo=="Masculino"){
                     resultado=(65-idade)
