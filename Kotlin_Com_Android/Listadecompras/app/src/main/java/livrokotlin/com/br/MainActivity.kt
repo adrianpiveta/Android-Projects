e22d29af8bb2fc0287a9e42ee26bfd3f934cbed0
adrianpiveta <danielpiveta257@gmail.com>
2022-01-19 19:23:58 -0300

    Criação lista compras

---
+++ Kotlin_Com_Android/Listadecompras/app/src/main/java/livrokotlin/com/br/MainActivity.kt
@@ -0,0 +1,20 @@
+package livrokotlin.com.br
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) //define xml que refere-se a essa classe
+        //val list_view_produtos = findViewById<View>(R.id.list_view_produto)
+        //foi eliminado
+
+        val produtosAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+
+        list_view_produtos.adapter = produtosAdapter
+
+    }
+}
